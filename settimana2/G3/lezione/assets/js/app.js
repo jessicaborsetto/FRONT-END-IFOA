d03b5a067e4e72568d6214bb70ba49842820e3ef
jessicaborsetto <bjessi2002@gmail.com>
2023-11-02 15:00:38 +0100

    settimana2_G3_pratica

--- settimana2/G3/lezione/assets/js/app.js
+++ settimana2/G3/lezione/assets/js/app.js
@@ -4,9 +4,10 @@
 1. leggere il campo nome e memorizzarlo
 2. leggere il campo anno e memorizzarlo
 3. calcolare l'età 
-4. verificare la maggiore o minore età
-5. scrivere nell'html 
-6. cancellare il form
+4. controllare la validità del form
+5. verificare la maggiore o minore età
+6. scrivere nell'html 
+7. cancellare il form
 
 EVENTO SCATENANTE - click
 
@@ -21,14 +22,16 @@ let anno = document.getElementById('anno');
 let eta;
 let stato;
 let errore = document.getElementById('errore');
+let esegui;
 
 //per scatenare l'evento per convenzione si usa questo:
 //serve per dare ordine agli eventi
 
 function eventHandler(){
     leggiForm();
+    //esegui()
     //controllo form
-    if (controlla()){
+    if (controlla){
         calcolaEta();
         verifica();
         scrivi();
@@ -40,17 +43,27 @@ function eventHandler(){
 
 //creo una funzione di SOLO controllo, quando una funzione risulta false si ferma tutto fino a quando non dà true
  
-    function controlla() {
+ function controlla() {
         if (anno>=1900 && anno<=2023 && nome!='') {
             errore.innerHTML = '&nbsp;';
             return true;
         }else{
             errore.innerHTML='Attenzione! Compilare tutti i campi e inserire un numero tra 1900 e 2023';
-            return;
             return false;
         }
     }
- 
+
+
+ /*   function esegui(){
+        if (anno>=1900 && anno<=2023) {
+            if (nome !=''){
+                errore.innerHTML = '';
+                esegui = true;
+            }
+        }
+        return esegui;
+    }
+*/
 
 function leggiForm() {
     nome = nome.value;  
