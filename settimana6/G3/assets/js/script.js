879c87d5aa3e79b2813ea8c645ba208c564d5efb
jessicaborsetto <bjessi2002@gmail.com>
2023-12-09 14:48:58 +0100

    settimana6_G3

--- settimana6/G3/assets/js/script.js
+++ settimana6/G3/assets/js/script.js
@@ -7,6 +7,14 @@ fetch(url)
 
 function handleBooks(libri){
     libri.forEach(el => {
+
+        let schedaLibro = document.createElement('div');
+        schedaLibro.classList.add('modal');
+        schedaLibro.setAttribute('id', 'cardModal')
+
+        let modalBody = document.createElement('div');
+        modalBody.classList.add('modal-body')
+       
         let card = document.createElement('div');
         card.classList.add('card');
         card.classList.add('bookCard');
@@ -19,7 +27,6 @@ function handleBooks(libri){
         let cardBody = document.createElement('div');
         cardBody.classList.add('card-body');
         cardBody.style.display = "none";
-        
 
         let cardTitle = document.createElement('h5');
         cardTitle.classList.add('card-title');
@@ -33,37 +40,83 @@ function handleBooks(libri){
         let buttonDiv = document.createElement('div');
         buttonDiv.classList.add('buttonDiv');
 
-        let cart = document.createElement('i');
-        cart.classList.add('bi');
-        cart.classList.add('bi-cart');
-
         let button = document.createElement('a');
         button.classList.add('btn');
         button.classList.add('btn-primary');
         button.href = '#';
-        button.innerText = cart + 'vai al carrello'
+        button.innerText ='vai al carrello'
         button.style.display = "none";
-
-        button.appendChild(cart);
 
         let buttonScarta = document.createElement('a');
         buttonScarta.classList.add('btn');
         buttonScarta.classList.add('btn-danger');
         buttonScarta.classList.add('btnInfo');
         buttonScarta.innerText = 'Info'
+        buttonScarta.setAttribute('id', 'cardModal')
         
-        buttonScarta.addEventListener('click', function() {
-            // Inverte la visibilità di cardBody associato a questo pulsante
-            if (cardBody.style.display === 'none' &&  button.style.display === "none") {
-                cardBody.style.display = 'inline';
-                button.style.display = "inline";
-            } else {
-                cardBody.style.display = 'none';
-                button.style.display = "none";
-            }
+        buttonScarta.addEventListener('click', function () {
+            // Creare il modale dinamicamente in JavaScript
+            let modal = document.createElement('div');
+            modal.classList.add('modal');
+            modal.classList.add('fade');
+            modal.setAttribute('id', 'dynamicCardModal');
+            modal.setAttribute('tabindex', '-1');
+            modal.setAttribute('role', 'dialog');
+
+            let modalDialog = document.createElement('div');
+            modalDialog.classList.add('modal-dialog');
+            modalDialog.setAttribute('role', 'document');
+
+            let modalContent = document.createElement('div');
+            modalContent.classList.add('modal-content');
+
+            let modalHeader = document.createElement('div');
+            modalHeader.classList.add('modal-header');
+
+            let modalTitle = document.createElement('h5');
+            modalTitle.classList.add('modal-title');
+            modalTitle.innerText = el.title;
+
+            let closeButton = document.createElement('button');
+            closeButton.classList.add('close');
+            closeButton.setAttribute('type', 'button');
+            closeButton.setAttribute('data-dismiss', 'modal');
+            closeButton.setAttribute('aria-label', 'Close');
+
+            closeButton.addEventListener('click', function () {
+                modal.remove(); // Rimuovi il modale al clic sulla chiusa
+                document.body.classList.remove('modal-open');
+            });
+
+            let closeIcon = document.createElement('span');
+            closeIcon.setAttribute('aria-hidden', 'true');
+            closeIcon.innerHTML = '&times;';
+
+            let modalBody = document.createElement('div');
+            modalBody.classList.add('modal-body');
+            modalBody.innerHTML = `
+                <p>${el.category}</p>
+                <img src="${el.img}" class="img-fluid" alt="${el.title}">
+            `;
+
+            modalHeader.appendChild(modalTitle);
+            closeButton.appendChild(closeIcon);
+            modalHeader.appendChild(closeButton);
+            modalContent.appendChild(modalHeader);
+            modalContent.appendChild(modalBody);
+            modalDialog.appendChild(modalContent);
+            modal.appendChild(modalDialog);
+
+            // Aggiungere il modale dinamicamente al body
+            document.body.appendChild(modal);
+
+            // Aggiungere la classe 'show' per mostrare il modale
+            modal.classList.add('show');
+            modal.style.display = 'block';
+            document.body.classList.add('modal-open');
         });
 
-
+        
         card.appendChild(img)
         card.appendChild(cardBody)
         card.appendChild(buttonDiv)
@@ -78,3 +131,4 @@ function handleBooks(libri){
         
     });
 }
+
